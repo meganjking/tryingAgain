5301efebea07b831ff080569b5c1d88163ce32e7
meganjking <meganjking@gmail.com>
2016-08-02 13:31:51 -0500

    Getting it set up

--- src/main/java/trying/controllers/PokeController.java
+++ src/main/java/trying/controllers/PokeController.java
@@ -1,12 +1,15 @@
 package trying.controllers;
 
 import com.sun.org.apache.regexp.internal.RE;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import trying.domain.Pokemon;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -15,30 +18,37 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class PokeController {
-    Pokemon[] pokemons;
+    ArrayList<Pokemon> pokemons;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     private final AtomicLong counter = new AtomicLong(-1);
 
     public PokeController(){
-        pokemons = new Pokemon[2];
+        pokemons = new ArrayList<Pokemon>();
 //        pokemons[0] = new Pokemon(1,"Pikachu","Electric", 5);
 //        pokemons[1] = new Pokemon(2,"Charizard","Fire", 7);
     }
 
     @RequestMapping(value = "/pokemon", method = RequestMethod.GET)
-    public Pokemon[] getAllPokemon(){
+    public ArrayList<Pokemon> getAllPokemon(){
+        jdbcTemplate.query(
+                "SELECT id, name, type, strength  FROM characters",
+                (rs, rowNum) -> new Pokemon(rs.getInt("id"), rs.getString("name"),
+                        rs.getString("type"), rs.getInt("strength"))).forEach(pokemon -> pokemons.add(pokemon));
         return pokemons;
     }
 
-    @RequestMapping(value = "/pokemon", method = RequestMethod.POST)
-    public Pokemon postPokemon(@RequestParam(value = "name") String name,
-                               @RequestParam(value = "type") String type,
-                               @RequestParam(value = "strength") int strength){
-        int count2 = (int) counter.incrementAndGet();
-        pokemons[count2] = new Pokemon(count2,name,type,strength);
-
-        return pokemons[count2];
-    }
+//    @RequestMapping(value = "/pokemon", method = RequestMethod.POST)
+//    public Pokemon postPokemon(@RequestParam(value = "name") String name,
+//                               @RequestParam(value = "type") String type,
+//                               @RequestParam(value = "strength") int strength){
+//        int count2 = (int) counter.incrementAndGet();
+//        pokemons[count2] = new Pokemon(count2,name,type,strength);
+//
+//        return pokemons[count2];
+//    }
 
 
 }
